051d1c2388d025941a0e2545ac83a9d40d38de06
WojciechowskiMarek <53943301+WojciechowskiMarek@users.noreply.github.com>
2023-09-14 21:56:33 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 import pypyodbc as odbc
 from pymodbus.client.serial import ModbusSerialClient as ModbusClient
 import re
-import time as time11
+import time as time
 from datetime import datetime as dt
-from datetime import datetime,date,time
 import logging
 
 DRIVER_NAME = "SQL SERVER"
-SERVER_NAME = "YOUR SERVER NAME"
-DATABASE_NAME = "YOUR DATABASE NAME"
+SERVER_NAME = "YOUR SARVER NAME"
+DATABASE_NAME = "YOUr DATABASE NAME"
 
 connection_string = f"""
     DRIVER={{{DRIVER_NAME}}};
@@ -32,20 +31,18 @@ if len (Last_Max_ID_SQL) == 0: Last_Max_ID_SQL = 0 # if base empty id = 0
 ID_Value = int(Last_Max_ID_SQL) + 1
 def is_set(x, n):
     return (x & 2 ** n != 0) 
-    # a more bitwise- and performance-friendly version:
-    return x & 1 << n != 0
 while True:
     PV = client.read_holding_registers(0x4a,1,0x01) #PV value
     SV = client.read_holding_registers(0x4b,1,0x01) #SV value
     SP1 = client.read_holding_registers(0x00,1,0x01) #SP1 given value
     HIAL = client.read_holding_registers(0x01,1,0x01) # HIAL upper limit alarm
     LOAL = client.read_holding_registers(0x02,1,0x01) # LOAL upper limit alarm
-    DIAG = client.read_holding_registers(0x4d,1,0x01) # DIAG register DIAG 4d byte  ---> 1 bit -! OP1 (turned on heating)
-                                                                     #              ---> 2 bit -! OP2,
-                                                                     #              ---> 3 bit -! AU1 - error bit
-                                                                     #              ---> 4 bit -! AU2 - error bit,
-                                                                     #              ---> 5 bit -! MIO
-                                                                     # 0b11111000000000 - state of controller word 4d, without any alarms etc. 
+    DIAG = client.read_holding_registers(0x4d,1,0x01) # DIAG register                DIAG 4d byte  ---> 1 bit -! OP1 (turned on heating)
+                                                                                    #              ---> 2 bit -! OP2,
+                                                                                    #              ---> 3 bit -! AU1 - error bit
+                                                                                    #              ---> 4 bit -! AU2 - error bit,
+                                                                                    #              ---> 5 bit -! MIO
+                                                                                    # 0b11111000000000 - state of controller word 4d, without any alarms etc. 
     diag_register = int(DIAG.registers[0])
     OP1 = is_set(diag_register,8)
     OP2 = is_set(diag_register,9)
@@ -66,9 +63,9 @@ while True:
     seconds=time_now.second
     Date_full1 = time_now.strftime("%d-%m-%Y %H:%M:%S")    
     formatter_string = "%d-%m-%Y %H:%M:%S" 
-    datetime_object = datetime.strptime(Date_full1, formatter_string)
+    datetime_object = dt.strptime(Date_full1, formatter_string)
     Date_full = datetime_object.replace(year,month,day,hour,minute,seconds)
     cursor.execute("insert into Temperature(ID,PV,SV,HIAL,SP1,LOAL,OP1,OP2,AU1,AU2,MIO,Czas1) values (?,?,?,?,?,?,?,?,?,?,?,?)",(ID_Value,a,b,c,d,e,OP1,OP2,AU1,AU2,MIO,Date_full))
     cursor.commit()
     ID_Value = ID_Value + 1
-    time11.sleep(5)
+    time.sleep(5)
